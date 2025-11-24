9207598e70e0eb589c2f0dd97108ef7813fa66cf
Razvan10020 <lincarazvan07@gmail.com>
2025-11-25 01:28:55 +0200

    Added Cell and teritory classes for map initiation

--- src/main/java/entities/simulation/Cell.java
+++ src/main/java/entities/simulation/Cell.java
@@ -1,4 +1,51 @@
 package entities.simulation;
 
+import entities.Air;
+import entities.Animal;
+import entities.Plant;
+import entities.Soil;
+import entities.Water;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Represents a single cell in the territory grid.
+ */
+@Getter
+@Setter
 public class Cell {
+    private Soil soil;
+    private Air air;
+    private Plant plant;
+    private Water water;
+    private Animal animal;
+
+    /**
+     * Constructor for the Cell class.
+     */
+    public Cell() {
+        this.soil = null;
+        this.air = null;
+        this.plant = null;
+        this.water = null;
+        this.animal = null;
+    }
+
+    /**
+     * Gets the number of entities in the cell.
+     * @return The number of entities.
+     */
+    public int getNumberOfEntities() {
+        int count = 0;
+        if (plant != null) {
+            count++;
+        }
+        if (animal != null) {
+            count++;
+        }
+        if (water != null) {
+            count++;
+        }
+        return count;
+    }
 }
--- src/main/java/entities/simulation/Territory.java
+++ src/main/java/entities/simulation/Territory.java
@@ -1,4 +1,44 @@
 package entities.simulation;
 
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Represents the territory grid.
+ */
+@Getter
+@Setter
 public class Territory {
+    private Cell[][] grid;
+    private int rows;
+    private int cols;
+
+    /**
+     * Constructor for the Territory class.
+     * @param rows The number of rows in the territory.
+     * @param cols The number of columns in the territory.
+     */
+    public Territory(final int rows, final int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.grid = new Cell[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                grid[i][j] = new Cell();
+            }
+        }
+    }
+
+    /**
+     * Gets the cell at the specified coordinates.
+     * @param x The x-coordinate of the cell.
+     * @param y The y-coordinate of the cell.
+     * @return The cell at the specified coordinates.
+     */
+    public Cell getCell(final int x, final int y) {
+        if (x >= 0 && x < rows && y >= 0 && y < cols) {
+            return grid[x][y];
+        }
+        return null;
+    }
 }
